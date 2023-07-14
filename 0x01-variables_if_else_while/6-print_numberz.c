0d29d4109cbc636d71aeba0afed9cf1e2ee6d3b2
NGONORMALIA <maliangonor00@gmail.com>
2023-07-14 14:08:19 -0700

    My command with putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point to print all the numbers without use prinf
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{ putchar(i); }
+	putchar('\n');
+	return (0);
+}
