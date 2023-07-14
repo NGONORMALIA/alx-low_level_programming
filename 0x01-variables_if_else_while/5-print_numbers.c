9367c4ed8aeb15405855099d38d3a825bb0ca84a
NGONORMALIA <maliangonor00@gmail.com>
2023-07-14 13:53:06 -0700

    My code to print all single numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point to print single digit number in base 10
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{ printf("%d", i); }
+	putchar('\n');
+	return (0);
+}
