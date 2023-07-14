b27b3981d3fe3e695c86b8bfb42909f93f4da09c
NGONORMALIA <maliangonor00@gmail.com>
2023-07-14 13:12:29 -0700

    My code to print in the same the alphabet in lower and upper case

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point to print alphabet in lower and upper case
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alpha;
+
+	for (alpha = 'a'; alpha = 'z'; alpha++)
+	{ putchar(alpha); }
+	for (alpha = 'A'; alpha = 'Z'; alpha++)
+	{ putchar(alpha); }
+	putchar('\n');
+	return (0);
+}
