15ac6fe97d32a67bf6714b135026e2fa6afc0a79
NGONORMALIA <maliangonor00@gmail.com>
2023-07-18 19:10:01 -0700

    My appending

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * add - to append two value
+ * @n: first iput value
+ * @w: second input value
+ * Return: Always return the result of adding
+ */
+int add(int n, int w)
+{
+	return (n + w);
+}
