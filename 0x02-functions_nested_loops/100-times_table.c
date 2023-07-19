a6d8667575c6c0440ea6146d39d8ec4bccd5694a
NGONORMALIA <maliangonor00@gmail.com>
2023-07-19 06:21:30 -0700

    My error

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -9,35 +9,36 @@ void print_times_table(int n)
 {
 	int i, j, k;
 
-	if (n > 15 || n < 0)
-		break;
-	for (i = 0; i <= n; i++)
+	if (n <= 15 && n >= 0)
 	{
-		_putchar(48);
-		for (j = 1; j <= n; j++)
+		for (i = 0; i <= n; i++)
 		{
-			k = i * j;
-			_putchar(',');
-			_putchar(' ');
-			if (k <= 9)
+			_putchar(48);
+			for (j = 1; j <= n; j++)
 			{
+				k = i * j;
+				_putchar(',');
 				_putchar(' ');
-				_putchar(' ');
-				_putchar(k + 48);
+				if (k <= 9)
+				{
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(k + 48);
+				}
+				else if (k <= 99)
+				{
+					_putchar(' ');
+					_putchar((k / 10) + 48);
+					_putchar((k % 10) + 48);
+				}
+				else
+				{
+					_putchar((k / 100) + 48);
+					_putchar(((k / 10) % 10) + 48);
+					_putchar((k % 10) + 48);
+				}
 			}
-			else if (k <= 99)
-			{
-				_putchar(' ');
-				_putchar((k / 10) + 48);
-				_putchar((k % 10) + 48);
-			}
-			else
-			{
-				_putchar((k / 100) + 48);
-				_putchar(((k / 10) % 10) + 48);
-				_putchar((k % 10) + 48)
-			}
+			_putchar('\n');
 		}
-		_putchar('\n');
 	}
 }
