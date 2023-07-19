f9c0aeee076e49f428c1df9d366df1df69a9e999
NGONORMALIA <maliangonor00@gmail.com>
2023-07-19 06:06:50 -0700

    My error

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+ * print_times_table - to print the alphabet
+ * Return: times tables
+ * @n: input valueof the time table
+ */
+void print_times_table(int n)
+{
+	int i, j, k;
+
+	if (n > 15 || n < 0)
+		break;
+	for (i = 0; i <= n; i++)
+	{
+		_putchar(48);
+		for (j = 1; j <= n; j++)
+		{
+			k = i * j;
+			_putchar(',');
+			_putchar(' ');
+			if (k <= 9)
+			{
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(k + 48);
+			}
+			else if (k <= 99)
+			{
+				_putchar(' ');
+				_putchar((k / 10) + 48);
+				_putchar((k % 10) + 48);
+			}
+			else
+			{
+				_putchar((k / 100) + 48);
+				_putchar(((k / 10) % 10) + 48);
+				_putchar((k % 10) + 48)
+			}
+		}
+		_putchar('\n');
+	}
+}
