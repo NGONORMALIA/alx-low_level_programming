205e29f3175e04f1f9f13534fb5f99c7ba7a6bd2
NGONORMALIA <maliangonor00@gmail.com>
2023-07-18 19:59:50 -0700

    My code

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - fibonaci number
+ * Return: the value
+ */
+int main(void)
+{
+	long int a, b, c, fn, afn;
+
+	a = 1;
+	b = 2;
+	fn = afn = 0;
+	while (fn <= 4000000)
+	{
+		fn = a + b;
+		a = b;
+		b = fn;
+		if ((a % 2) == 0)
+		{
+			afn += a;
+		}
+	}
+	printf("%ld\n", afn);
+	return (0);
+}
