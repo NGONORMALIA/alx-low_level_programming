672e2b753f05faa8e2f4f6dbd6009a6dc68dc155
NGONORMALIA <maliangonor00@gmail.com>
2023-08-25 18:00:55 -0700

    Another

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -11,7 +11,7 @@ list_t *add_node(list_t **head, const char *str)
 	size_t n;
 	list_t *new_node;
 
-	*new_node = malloc(sizeof(list_t));
+	new_node = malloc(sizeof(list_t));
 	if (new_node == NULL)
 		return (NULL);
 	new_node->str = strdup(str);
